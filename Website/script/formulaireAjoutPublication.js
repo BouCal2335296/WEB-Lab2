b2f5cfeb36158d6ee9f5268da609a53be7f74130
Calvin Bourdet <143567950+BouCal2335296@users.noreply.github.com>
2025-02-23 17:45:13 -0500

    Ajout page formulaire
    Bug :
    -La mise en page n'est pas respecter lors de l'envoi des commentaires
    
    -Si on supprime manuellement des publication de db.json, ils s'affichent toujours, et si on post une nouvelle publication, ils se recréer tout seul. Obligé de redémarrer l'application pour appliquer la suppression

--- Website/script/formulaireAjoutPublication.js
+++ Website/script/formulaireAjoutPublication.js
@@ -1,25 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form');
+    const btnAjouter = document.getElementById('ajouter');
+    const btnConfirmer = document.getElementById('confirmer');
 
-    if (form) {
         form.addEventListener('submit', function (event) {
             event.preventDefault();
             const titre = document.getElementById('titre').value;
             const auteur = document.getElementById('auteur').value;
             const contenu = document.getElementById('contenu').value;
 
+            cacherBoutonAjouter();
+            form.addEventListener('click', function () {
+                fetch('http://localhost:3000/publication', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ titre, auteur, contenu })
+                })
+                window.location.href = "principale.html";
+            });
 
-            fetch('http://localhost:3000/publication', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ titre, auteur, contenu })
-            })
+
         });
-        window.location.href = "principale.html";
-    }
 });
+
+function cacherBoutonAjouter() {
+    $("#ajouter").fadeOut(200, function () {
+        $("#ajouter").hide();
+        afficherBoutonConfirmer();
+    });
+}
+function afficherBoutonConfirmer() {
+    $("#confirmer").removeAttr('hidden').hide().fadeIn(200);
+    console.log("test1");
+}
 
 
 
