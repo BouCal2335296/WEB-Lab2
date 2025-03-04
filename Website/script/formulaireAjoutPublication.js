f2d4d927cd0f5c8cb2e8232dc5aac16460f11d81
Calvin Bourdet <143567950+BouCal2335296@users.noreply.github.com>
2025-03-03 19:34:42 -0500

    modif ajoutPublication

--- Website/script/formulaireAjoutPublication.js
+++ Website/script/formulaireAjoutPublication.js
@@ -3,67 +3,51 @@ document.addEventListener('DOMContentLoaded', function () {
     const btnAjouter = document.getElementById('ajouter');
     const btnConfirmer = document.getElementById('confirmer');
 
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
-            const titre = document.getElementById('titre').value;
-            const auteur = document.getElementById('auteur').value;
-            const contenu = document.getElementById('contenu').value;
-
-            cacherBoutonAjouter();
-            form.addEventListener('click', function () {
-                fetch('http://localhost:3000/publication', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ titre, auteur, contenu })
-                })
-                window.location.href = "principale.html";
-            });
-
-
-        });
-});
-
-function cacherBoutonAjouter() {
-    $("#ajouter").fadeOut(200, function () {
-        $("#ajouter").hide();
-        afficherBoutonConfirmer();
+    $("#message").dialog({
+        closeOnEscape: false,
+        autoOpen: false,
+        modal: true,  // Empêche l'interaction avec le reste du site
+        buttons: {
+            "Confirmer": function () {
+                //ajouterPublication();
+                $(this).dialog("close");
+            },
+            "Annuler": function () {
+                $(this).dialog("close");
+                $("#ajouter").fadeIn(200); // Réaffiche le bouton Ajouter si l'utilisateur annule
+            }
+        }
     });
-}
-function afficherBoutonConfirmer() {
-    $("#confirmer").removeAttr('hidden').hide().fadeIn(200);
-    console.log("test1");
-}
 
+    btnAjouter.addEventListener('click', function (event) {
+        event.preventDefault();
+        $("#ajouter").fadeOut(200, function () {
+            $("#message").dialog("open");
+        });
+    });
 
+    function ajouterPublication() {
+        const titre = document.getElementById('titre').value;
+        const auteur = document.getElementById('auteur').value;
+        const contenu = document.getElementById('contenu').value;
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function ajouterPublication()
-// {
-
-
-//     fetch("http://localhost:3000/publication",
-//         {
-//             method: "POST",
-//             body: JSON.stringify({
-//                 id: 12,
-//                 titre: "blablabla",
-//                 auteur: "blablabla",
-//                 date: "blablabla",
-//                 contenu: "blablabla"
-//             })
-//         }
-//     )
-// }
+        fetch('http://localhost:3000/publication', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ titre, auteur, contenu })
+        }).then(response => {
+            if (response.ok) {
+                window.location.href = "principale.html";
+            } else {
+                alert("Erreur lors de la publication");
+                $("#ajouter").fadeIn(200);
+            }
+        }).catch(error => {
+            console.error('Erreur:', error);
+            alert("Impossible de contacter le serveur");
+            $("#ajouter").fadeIn(200);
+        });
+    }
+});
--- Website/script/index.js
+++ Website/script/index.js
@@ -21,44 +21,22 @@ function afficherCommentaire()
             {
                 let commentaireContainer = $("#contenu");
                 commentaireContainer.empty();
-                let style = true;
                 let commentaire;
                 data.forEach(comment => {
-                        if(style == true)
-                        {
-                            commentaire = `
-                            <div class="row pt-4">
-                                <div class="col-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle border-dark rounded-4 glow-effect bgBlue" viewBox="0 0 16 16">
-                                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                                    </svg>
-                                </div>
-                                <div class="col-11">
-                                    <p class="Open-Sans-font text-white">${comment.contenu}</p>
-                                </div>
-                            </div>`;
-                            style = false;
-                        }
-                        if(style == false)
-                        {
-                            commentaire = `
-                            <div class="row pt-4">
-                                <div class="col-1"></div>
-                                    <div class="col-1">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle border-dark rounded-4 glow-effect bgBlue" viewBox="0 0 16 16">
-                                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                                        </svg>
-                                    </div>
-                                    <div class="col-10">
-                                        <p class="Open-Sans-font text-white">${comment.contenu}</p>
-                                    </div>
-                                </div>`;
-                        }
-                        commentaireContainer.append(commentaire);
-                    }
-                );
+                    commentaire = `
+                    <div class="row pt-4">
+                        <div class="col-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle border-dark rounded-4 glow-effect bgBlue" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                            </svg>
+                        </div>
+                        <div class="col-11">
+                            <p class="Open-Sans-font text-white">${comment.contenu}</p>
+                        </div>
+                    </div>`;
+                    commentaireContainer.append(commentaire);
+                });
             }
         );
 }
